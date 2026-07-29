2563374877c6c2fd262eec2e5de307f65e8c9dd8
YIFAN JIA <jif@outlook.com>
2026-07-29 05:06:36 +0000

    phase 2: eks cluster with arm64 managed node group

---
+++ terraform/eks.tf
@@ -0,0 +1,37 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = var.project
+  kubernetes_version = "1.33"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  endpoint_public_access = true
+
+  # Grants cluster-admin to the IAM identity running terraform,
+  # via EKS Access Entries (aws-auth ConfigMap is no longer used).
+  enable_cluster_creator_admin_permissions = true
+
+  addons = {
+    coredns                = {}
+    kube-proxy             = {}
+    vpc-cni                = { before_compute = true }
+    eks-pod-identity-agent = {}
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t4g.large"]
+      ami_type       = "AL2023_ARM_64_STANDARD"
+      capacity_type  = "ON_DEMAND"
+
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      disk_size = 40
+    }
+  }
+}
